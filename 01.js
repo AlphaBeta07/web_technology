63d4dc3d1a5a6c5e2c56432b0e804720c3b7e681
AlphaBeta07 <anishlandage007@gmail.com>
2026-01-14 15:32:32 +0530

    commit

--- 01.js
+++ 01.js
@@ -2,7 +2,18 @@
 //     a = 20;
 // console.log(a);
 
-var a = 10; a= 200;
-let ab=20; ab = 300;
-console.log(a);
-console.log(ab);
+// var a = 10; a= 200;
+// let ab=20; ab = 300;
+// console.log(a);
+// console.log(ab);
+
+// {
+//     var a = 10; a= 200;
+//     let ab=20; ab = 300;
+//     console.log(a);
+//     console.log(ab);
+
+// }
+
+
+ 
---
+++ conversion_opretaion.js
@@ -0,0 +1,10 @@
+let num = "33";
+// let string_num = String(num);
+// console.log(string_num);
+
+// console.log(typeof(string_num));
+
+let num_string = Number(num);
+console.log(num_string);
+
+console.log(typeof(num_string));
