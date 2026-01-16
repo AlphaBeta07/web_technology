d0ddd20cb09a9a2f6069c66da40fb04b5d2994b8
AlphaBeta07 <anishlandage007@gmail.com>
2026-01-16 15:56:01 +0530

    commit

---
+++ lab/activity.js
@@ -0,0 +1,49 @@
+// activity 1
+// display student info let studentName, Number studenRoll, String studentEmail
+let studentName = "Anish" ;
+let studenRoll = "23UAM005";
+let studentEmail = "anishlandage007@gmail.com";
+
+console.log("Ehnter name : ", studentName);
+console.log("Enter roll number : ", studenRoll);
+console.log("Enter email : ", studentEmail);
+
+document.writeln("<h3>" + "Enter name : " + studentName + "</h3>");
+document.writeln("<h3>" + "Enter roll number : "+ studenRoll + "</h3>");
+document.writeln("<h3>" + "Enter email : " +  studentEmail + "</h3>");
+// activity 2
+// check is number is even or odd
+// let a = 10;
+// if (a % 2 == 0)
+//     console.log("even");
+// else
+//     console.log("odd");
+    
+
+// activity 3
+// check student pass or fail
+// let math = 87;
+// let phy = 78;
+// let chem = 85;
+// let grade = (math + phy + chem) / 3;
+// console.log(grade);
+// if (grade >= 40)
+//     console.log("pass");
+// else
+//     console.log("fail");
+
+    
+
+// activity 4
+// print display number 1 - 10
+for (let i = 1; i <= 10; i++){
+    document.writeln("<h3>"+i+"</h3>");
+    console.log(i);
+}
+// activity 5
+let x =10;
+y = x;
+x =2001;
+console.log(x, y);
+
+//activity 6
---
+++ lab/exp1.js
@@ -0,0 +1,15 @@
+
+
+
+// const a = 44;
+// a = 55;
+// console.log(a);
+
+let an =100;
+var ann = 200;
+{
+    let an = 10;
+    var ann=19;
+    console.log(an, ann);
+}
+console.log(an, ann);
