05934ba6e609fd00b7f8e8a369aa4f641435c20f
AlphaBeta07 <anishlandage007@gmail.com>
2026-04-28 22:08:48 +0530

    commmit

---
+++ classwork/websocket/server.js
@@ -0,0 +1,24 @@
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 3000 });
+
+console.log("WebSocket server is running on http://localhost:3000");
+
+wss.on("connection", function connection(ws) {
+    console.log("New client connected");
+
+    // ws.send("Welcome to WebSocket Server");
+
+    ws.on("message", function incoming(message) {
+        console.log("Received:", message.toString());
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+});
